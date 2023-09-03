763472b045160f84f3f39b8493c3662f3a485837
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-09-03 10:17:28 +0300

    Symbol Currency.java

--- Homework_202308_30/Currency.java
+++ Homework_202308_30/Currency.java
@@ -1,7 +1,17 @@
 package Homework_202308_30;
 public enum Currency {
-    EUR,
-    USD,
-    GBP,
-    INR,
+    EUR ("$"),
+    USD ("€"),
+    GBP ("£"),
+    INR ("₹");
+
+    private final String symbol;
+
+    Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
